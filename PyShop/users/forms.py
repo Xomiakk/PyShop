e98478db0757382f1dccac5d19d7e702fead7e6b
Xomiakk <vlad1matviychuk@gmail.com>
2023-01-04 22:47:18 +0200

    First stage

---
+++ PyShop/users/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm as _UserCreationForm
+from django.contrib.auth import get_user_model
+from users.models import User
+
+User = get_user_model()
+
+
+class UserCreationForm(_UserCreationForm):
+	class Meta(_UserCreationForm.Meta):
+		model = User
